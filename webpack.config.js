c442445951f4f48fb7cd9c23cc3e95ad4ebbb0bd
Vadim <v_a_d@inbox.ru>
2021-04-19 22:11:24 +0300

    update webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,33 +1,65 @@
 const webpack = require('webpack');
 const path = require('path');
 const Copy = require('copy-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const fs = require('fs');
+
+const pageNames = fs.readdirSync('./pages')
+  .filter((value) => value.includes('.html'))
+  .map((value) => value.replace('.html', ''));
+
+const entryPoints = pageNames.reduce((acc, value) => ({ ...acc, ...{ [value]: path.resolve(__dirname, "pages", `${value}.js`) } }), {});
+
+const copyPaths = pageNames.map((name) => ({
+  from: path.resolve(__dirname, `pages/${name}.html`),
+  to: path.resolve(__dirname, `dist/${name}`),
+}));
 
 module.exports = {
-    entry: {
-      // index: path.resolve(__dirname, "pages", "index.js"),
-      main: path.resolve(__dirname, "pages", "main.js"),
-    },
-    output: {
-        filename: `[name].js`,
-        path: path.join(__dirname, 'dist')
-    },
-    module: {
-        rules: [
-            { 
-              test: /\.html$/, 
-              use: [
-                {
-                  loader: 'bemdecl-to-fs-loader',
-                  options: { levels: ['desktop'], extensions: ['css', 'js'] } // Add css and js files of BEM entities to bundle
-                },
-                { loader: 'html2bemdecl-loader' } // First, convert HTML to bem DECL format
-              ] },
-            { test: /\.css$/, loader: 'style-loader!css-loader' }
+  entry: entryPoints,
+  output: {
+    filename: `[name]/[name].js`,
+    path: path.join(__dirname, 'dist')
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: ({ chunk }) => `${chunk.name}/${chunk.name}.css`,
+    }),
+    new Copy(copyPaths),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: 'bemdecl-to-fs-loader',
+            options: { levels: ['desktop'], extensions: ['css', 'js'] } // Add css and js files of BEM entities to bundle
+          },
+          {
+            loader: 'html2bemdecl-loader'
+          } // First, convert HTML to bem DECL format
         ]
-    },
-    plugins: [
-        new Copy([
-            { from: path.resolve(__dirname, 'pages'), test: /\.html$/, to: path.resolve(__dirname, "dist") }
-        ])
+      },
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+          },
+          'css-loader'
+        ],
+      },
+      {
+        test: /\.(gif|png|jpg|jpeg|svg)?$/,
+        loader: 'file-loader',
+        options: {
+          name: '[name].[ext]',
+          outputPath: '../dist/img/',
+          esModule: false,
+          publicPath: '../img/'
+        },
+      },
     ]
+  },
 }
