eabc50b048caf8823be8b74789ae5e3a5e78f935
Vadim <v_a_d@inbox.ru>
2021-04-22 21:10:45 +0300

    update temp files

--- temp.js
+++ temp.js
@@ -1,15 +1,8 @@
-office-phone +
-order-of-work +
-owl-carousel +
-page +
-page-header +
+
+ +
 portfolio +
 service-menu +
 service-phone +
 services +
 
-/* const entryPoints = fs.readdirSync('./pages')
-.filter((value) => value.includes('.html'))
-.map((value) => value.replace('.html', ''))
-.reduce((acc, value) => ({ ...acc, ...{ [value]: path.resolve(__dirname, "pages", `${value}.js`) }}), {});
-console.log(entryPoints); */
+owl-carousel +
