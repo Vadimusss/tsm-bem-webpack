f1247a52e638fe50ccebd0269ff8f9b9d1aff896
Vadim <v_a_d@inbox.ru>
2021-07-26 23:41:03 +0300

    Add main-menu block

---
+++ src/common/main-menu/main-menu.js
@@ -0,0 +1,6 @@
+const button = document.querySelector('.mobile-main-menu-button');
+const menu = document.querySelector('.page-header__main-menu');
+button.addEventListener('click', () => {
+  menu.classList.toggle('page-header__main-menu_visible');
+  button.classList.toggle('mobile-main-menu-button_close');
+});
