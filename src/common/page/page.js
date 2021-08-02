004c74816b98b207c8a6cb2e45615dee87ffd814
root <root@VADIM.localdomain>
2021-08-02 12:20:22 +0300

    add page block js

---
+++ src/common/page/page.js
@@ -0,0 +1,9 @@
+/* Закрываем ссылки от поисковиков */
+
+$('.hl').replaceWith(function () {
+  const href = $(this).data('link') ? `href="${$(this).data('link')}"` : '';
+  const tagClass = this.className ? `class="${this.className}"`.replace(' hl', '') : '';
+  const title = $(this).text() ? `title="${$(this).text()}"` : '';
+  const content = $(this).html();
+  return `<a ${href} ${tagClass} ${title}>${content}</a>`;
+});
