349270809ae3920b64e83281719d64f45e81d1f2
root <root@VADIM.localdomain>
2021-08-03 17:16:13 +0300

    add unfolding-block block

---
+++ src/common/unfolding-block/unfolding-block.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function () {
+  document.querySelectorAll('.unfolding-block__button').forEach((button) => {
+    button.addEventListener('click', (e) => {
+      e.target.classList.toggle('unfolding-block__button_fold');
+      e.target.nextElementSibling.classList.toggle('unfolding__content_visible');
+    })
+  });
+}, false);
