23396895b9b31fc2eceffd14bc69532e33b6d3a0
Vadim <v_a_d@inbox.ru>
2021-08-30 16:56:14 +0300

    made a lazy loading of yandex maps in zones block

---
+++ src/common/zones/zones.js
@@ -0,0 +1,21 @@
+/* eslint-disable no-undef */
+document.addEventListener('DOMContentLoaded', () => {
+  const mapContainer = document.getElementsByClassName('zones__live-map')[0];
+  const optionsMap = {
+    once: true,
+    passive: true,
+    capture: true,
+  };
+
+  const mapLoaded = false;
+  function startLazyMap() {
+    if (!mapLoaded) {
+      const script = document.createElement('script');
+      script.setAttribute('src', 'https://api-maps.yandex.ru/services/constructor/1.0/js/?um=constructor%3A0cd4096024309055b1d48e7d0647823821d80d5947240f0fff5b2e5e95e532f1&amp;width=100%25&amp;height=100%25&amp;lang=ru_RU&amp;scroll=true');
+      mapContainer.appendChild(script);
+    }
+  }
+
+  mapContainer.addEventListener('mouseover', startLazyMap, optionsMap);
+  mapContainer.addEventListener('touchstart', startLazyMap, optionsMap);
+}, false);
