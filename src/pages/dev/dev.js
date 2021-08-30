ff52967b5243724aa674a3b51b92bf9a710e8647
Vadim <v_a_d@inbox.ru>
2021-08-30 17:20:51 +0300

    add page for development

---
+++ src/pages/dev/dev.js
@@ -0,0 +1,22 @@
+/* blocks connected first (instead of bemdecl) */
+import '../../common/header/header.css';
+import '../../common/button/button.css';
+import '../../common/menu/menu.css';
+
+import './dev.html';
+import '../../fonts/fonts.css';
+
+import '../../img/brands_buderus.jpg';
+import '../../img/brands_baxi.jpg';
+import '../../img/brands_viessmann.jpg';
+import '../../img/brands_vaillant.jpg';
+import '../../img/brands_protherm.jpg';
+import '../../img/brands_wolf.jpg';
+import '../../img/brands_hansa.jpg';
+import '../../img/brands_acv.jpg';
+import '../../img/brands_sime.png';
+import '../../img/brands_bosch.png';
+import '../../img/brands_lamborghini.png';
+import '../../img/brands_lemax.png';
+import '../../img/sro_01.jpg';
+import '../../img/sro_02.jpg';
