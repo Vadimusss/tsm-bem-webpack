ecda23de50ecc1699cf4a5dfee55962b7dee5ec5
Vadim <v_a_d@inbox.ru>
2021-09-01 16:53:57 +0300

    disable eslint no-undef rule

--- src/common/callback-modal/callback-modal.js
+++ src/common/callback-modal/callback-modal.js
@@ -1,11 +1,12 @@
-import '../jquery-modal/jquery-modal.js';
+/* eslint-disable no-undef */
+import '../jquery-modal/jquery-modal';
 import '../jquery-modal/jquery-modal.css';
 
-document.addEventListener('DOMContentLoaded', function () {
+document.addEventListener('DOMContentLoaded', () => {
   $.modal.defaults = {
     escapeClose: true,
     clickClose: true,
     showClose: true,
-    blockerClass: 'callback-modal__blocker',  // CSS class added to the overlay (blocker).
+    blockerClass: 'callback-modal__blocker', // CSS class added to the overlay (blocker).
   };
 }, false);
