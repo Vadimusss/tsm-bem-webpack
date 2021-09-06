d63d307f6dba9ef371a22ed1930a40552fa51355
Vadim <v_a_d@inbox.ru>
2021-09-06 17:13:00 +0300

    update development template

--- src/pages/dev/dev.js
+++ src/pages/dev/dev.js
@@ -2,6 +2,9 @@
 import '../../common/header/header.css';
 import '../../common/button/button.css';
 import '../../common/menu/menu.css';
+import '../../common/callback/callback.css';
+import '../../common/breadcrumbs/breadcrumbs.css';
+import '../../shop/breadcrumbs/breadcrumbs.css';
 
 import './dev.html';
 import '../../fonts/shop-fonts.css';
