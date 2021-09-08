0edfd9431c70a49c88754a8fc5f4e2cf75ab5398
Vadim <v_a_d@inbox.ru>
2021-09-08 11:37:10 +0300

    add shop-category page template

---
+++ src/pages/shop-category.js
@@ -0,0 +1,11 @@
+/* blocks connected first (instead of bemdecl) */
+import '../common/header/header.css';
+import '../common/button/button.css';
+import '../common/menu/menu.css';
+import '../common/services/services.css';
+import '../common/callback/callback.css';
+import '../common/breadcrumbs/breadcrumbs.css';
+import '../shop/breadcrumbs/breadcrumbs.css';
+
+import './shop-category.html';
+import '../fonts/shop-fonts.css';
