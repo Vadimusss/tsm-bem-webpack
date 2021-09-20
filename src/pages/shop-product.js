8cf23a4bf8504cff33207c6b24c9f430d26659eb
Vadim <v_a_d@inbox.ru>
2021-09-20 15:20:58 +0300

    add shop-product page

---
+++ src/pages/shop-product.js
@@ -0,0 +1,11 @@
+import '../common/header/header.css';
+import '../common/button/button.css';
+import '../common/menu/menu.css';
+import '../common/location/location.css';
+import '../common/services/services.css';
+import '../common/callback/callback.css';
+import '../common/breadcrumbs/breadcrumbs.css';
+import '../shop/breadcrumbs/breadcrumbs.css';
+
+import './shop-product.html';
+import '../fonts/shop-fonts.css';
