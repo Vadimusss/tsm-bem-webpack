824f0ddc82b6ac9fe351caea4d373d7898e395d4
Vadim <v_a_d@inbox.ru>
2021-09-20 17:38:43 +0300

    separate tabs block

--- src/shop/shop-product/shop-product.js
+++ src/shop/shop-product/shop-product.js
@@ -5,25 +5,3 @@
 jQuery.fn.size = function () {
   return this.length;
 };
-
-document.addEventListener('DOMContentLoaded', () => {
-  const tabs = document.querySelector('.shop-product__tabs');
-  const tabsBtn = document.querySelectorAll('.shop-product__tabs-botton');
-  const tabsContent = document.querySelectorAll('.shop-product__tabs-content');
-
-  const tabsHandler = (path) => {
-    tabsContent.forEach(element => element.classList.remove('shop-product__tabs-content_active'));
-    document.querySelector(`[data-tabs-target="${path}"]`).classList.add('shop-product__tabs-content_active');
-  };
-
-  if (tabs) {
-    tabs.addEventListener('click', ({ target }) => {
-      if (target.classList.contains('shop-product__tabs-botton')) {
-        const { tabsPath } = target.dataset;
-        tabsBtn.forEach(element => element.classList.remove('shop-product__tabs-botton_active'));
-        document.querySelector(`[data-tabs-path="${tabsPath}"]`).classList.add('shop-product__tabs-botton_active');
-        tabsHandler(tabsPath);
-      }
-    });
-  }
-});
---
+++ src/shop/tabs/tabs.js
@@ -0,0 +1,22 @@
+/* eslint-disable no-undef */
+document.addEventListener('DOMContentLoaded', () => {
+  const tabs = document.querySelector('.tabs');
+  const tabsBtn = document.querySelectorAll('.tabs__botton');
+  const tabsContent = document.querySelectorAll('.tabs__content');
+
+  const tabsHandler = (path) => {
+    tabsContent.forEach(element => element.classList.remove('tabs__content_active'));
+    document.querySelector(`[data-tabs-target="${path}"]`).classList.add('tabs__content_active');
+  };
+
+  if (tabs) {
+    tabs.addEventListener('click', ({ target }) => {
+      if (target.classList.contains('tabs__botton')) {
+        const { tabsPath } = target.dataset;
+        tabsBtn.forEach(element => element.classList.remove('tabs__botton_active'));
+        document.querySelector(`[data-tabs-path="${tabsPath}"]`).classList.add('tabs__botton_active');
+        tabsHandler(tabsPath);
+      }
+    });
+  }
+});
