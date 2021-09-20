9d26f4ea6d5fafc7566e502e3818a9ff8dc91754
Vadim <v_a_d@inbox.ru>
2021-09-20 17:37:37 +0300

    fix linter error

--- src/common/page/page.js
+++ src/common/page/page.js
@@ -1,7 +1,7 @@
 /* eslint-disable no-undef */
 /* Закрываем ссылки от поисковиков */
 document.addEventListener('DOMContentLoaded', () => {
-  $('.hl').replaceWith(() => {
+  $('.hl').replaceWith(function callback() {
     const href = $(this).data('link') ? `href="${$(this).data('link')}"` : '';
     const tagClass = this.className ? `class="${this.className}"`.replace(' hl', '') : '';
     const title = $(this).text() ? `title="${$(this).text()}"` : '';
