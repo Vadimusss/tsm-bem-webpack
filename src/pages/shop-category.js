dfbba5bce785e9c9a8ee7630c69f781e51bd9d4a
Vadim <v_a_d@inbox.ru>
2021-09-20 15:23:40 +0300

    fix linter errors

--- src/pages/shop-category.js
+++ src/pages/shop-category.js
@@ -2,6 +2,7 @@
 import '../common/header/header.css';
 import '../common/button/button.css';
 import '../common/menu/menu.css';
+import '../common/location/location.css';
 import '../common/services/services.css';
 import '../common/callback/callback.css';
 import '../common/breadcrumbs/breadcrumbs.css';
