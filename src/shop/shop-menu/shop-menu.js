33c5c20c5d3a35b7a63b79b2afd8dce093a7cf6b
Vadim <v_a_d@inbox.ru>
2021-09-20 15:31:20 +0300

    add block existance check

--- src/shop/shop-menu/shop-menu.js
+++ src/shop/shop-menu/shop-menu.js
@@ -2,15 +2,17 @@
 document.addEventListener('DOMContentLoaded', () => {
   const categoriesBlocks = document.querySelectorAll('.shop-menu__category');
 
-  categoriesBlocks.forEach(block => block.addEventListener('click', ({ target }) => {
-    categoriesBlocks.forEach((element) => {
-      if (element !== target) {
-        element.classList.remove('shop-menu__category_open');
-        element.firstElementChild.classList.remove('shop-menu__subcategories-list_visible');
-      }
-    });
+  if (categoriesBlocks) {
+    categoriesBlocks.forEach(block => block.addEventListener('click', ({ target }) => {
+      categoriesBlocks.forEach((element) => {
+        if (element !== target) {
+          element.classList.remove('shop-menu__category_open');
+          element.firstElementChild.classList.remove('shop-menu__subcategories-list_visible');
+        }
+      });
 
-    target.classList.toggle('shop-menu__category_open');
-    target.firstElementChild.classList.toggle('shop-menu__subcategories-list_visible');
-  }));
+      target.classList.toggle('shop-menu__category_open');
+      target.firstElementChild.classList.toggle('shop-menu__subcategories-list_visible');
+    }));
+  }
 }, false);
