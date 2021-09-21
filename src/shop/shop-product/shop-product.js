88fd6841bacabb62d187b8ad9b76e5e815266725
Vadim <v_a_d@inbox.ru>
2021-09-21 17:26:41 +0300

    add script for change quantity buttons

--- src/shop/shop-product/shop-product.js
+++ src/shop/shop-product/shop-product.js
@@ -5,3 +5,22 @@
 jQuery.fn.size = function () {
   return this.length;
 };
+
+document.addEventListener('DOMContentLoaded', () => {
+  const decreaceButton = document.querySelector('.shop-product__order-quantity-button_type_decrease');
+  const incraseButton = document.querySelector('.shop-product__order-quantity-button_type_incrase');
+  const quantityInput = document.querySelector('.shop-product__quantity-input');
+
+  decreaceButton.addEventListener('click', (event) => {
+    event.preventDefault();
+    let currentInputValue = Number(quantityInput.value);
+    if (currentInputValue > 1) {
+      quantityInput.value = String(currentInputValue -= 1);
+    }
+  });
+  incraseButton.addEventListener('click', (event) => {
+    event.preventDefault();
+    let currentInputValue = Number(quantityInput.value);
+    quantityInput.value = String(currentInputValue += 1);
+  });
+}, false);
