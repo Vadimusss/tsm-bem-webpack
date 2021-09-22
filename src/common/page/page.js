10d8be0f07f89d400238375de31251be7acebc19
Vadim <v_a_d@inbox.ru>
2021-09-22 17:30:53 +0300

    add id attribute processing

--- src/common/page/page.js
+++ src/common/page/page.js
@@ -6,6 +6,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const tagClass = this.className ? `class="${this.className}"`.replace(' hl', '') : '';
     const title = $(this).text() ? `title="${$(this).text()}"` : '';
     const content = $(this).html();
-    return `<a ${href} ${tagClass} ${title}>${content}</a>`;
+    const id = this.id ? `id="${this.id}"` : '';
+    return `<a ${href} ${tagClass} ${title} ${id}>${content}</a>`;
   });
 }, false);
