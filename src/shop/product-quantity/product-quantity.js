e69eeb1c0f1aebb454d7656b9c253a1486e34e9a
Vadim <v_a_d@inbox.ru>
2021-11-01 15:36:29 +0300

    update product-quantity block js

--- src/shop/product-quantity/product-quantity.js
+++ src/shop/product-quantity/product-quantity.js
@@ -1,19 +1,29 @@
 /* eslint-disable no-undef */
 document.addEventListener('DOMContentLoaded', () => {
-  const decreaceButton = document.querySelector('.product-quantity__button_type_decrease');
-  const incraseButton = document.querySelector('.product-quantity__button_type_incrase');
-  const quantityInput = document.querySelector('.product-quantity__input');
+  const decreaseButtons = document.querySelectorAll('.product-quantity__button_type_decrease');
+  const incraseButtons = document.querySelectorAll('.product-quantity__button_type_incrase');
 
-  decreaceButton.addEventListener('click', (event) => {
-    event.preventDefault();
-    let currentInputValue = Number(quantityInput.value);
-    if (currentInputValue > 1) {
-      quantityInput.value = String(currentInputValue -= 1);
-    }
+  decreaseButtons.forEach((button) => {
+    const input = button.nextElementSibling;
+    button.addEventListener('click', (event) => {
+      if (event.target.classList.contains('product-quantity__button_prevent_true')) {
+        event.preventDefault();
+      }
+      const currentInputValue = parseInt(input.value, 10);
+      if (currentInputValue > 1) {
+        input.value = currentInputValue - 1;
+      }
+    });
   });
-  incraseButton.addEventListener('click', (event) => {
-    event.preventDefault();
-    let currentInputValue = Number(quantityInput.value);
-    quantityInput.value = String(currentInputValue += 1);
+
+  incraseButtons.forEach((button) => {
+    const input = button.previousElementSibling;
+    button.addEventListener('click', (event) => {
+      if (event.target.classList.contains('product-quantity__button_prevent_true')) {
+        event.preventDefault();
+      }
+      const currentInputValue = parseInt(input.value, 10);
+      input.value = currentInputValue + 1;
+    });
   });
-}, false);
+});
