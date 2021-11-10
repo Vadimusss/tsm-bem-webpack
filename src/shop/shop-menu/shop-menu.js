b24041a3528e21cf391eda44283b15135b321d10
Vadim <v_a_d@inbox.ru>
2021-11-10 15:09:36 +0300

    add target class check

--- src/shop/shop-menu/shop-menu.js
+++ src/shop/shop-menu/shop-menu.js
@@ -4,15 +4,17 @@ document.addEventListener('DOMContentLoaded', () => {
 
   if (categoriesBlocks) {
     categoriesBlocks.forEach(block => block.addEventListener('click', ({ target }) => {
-      categoriesBlocks.forEach((element) => {
-        if (element !== target) {
-          element.classList.remove('shop-menu__category_open');
-          element.firstElementChild.classList.remove('shop-menu__subcategories-list_visible');
-        }
-      });
+      if (target.classList.contains('shop-menu__category')) {
+        categoriesBlocks.forEach((element) => {
+          if (element !== target) {
+            element.classList.remove('shop-menu__category_open');
+            element.firstElementChild.classList.remove('shop-menu__subcategories-list_visible');
+          }
+        });
 
-      target.classList.toggle('shop-menu__category_open');
-      target.firstElementChild.classList.toggle('shop-menu__subcategories-list_visible');
+        target.classList.toggle('shop-menu__category_open');
+        target.firstElementChild.classList.toggle('shop-menu__subcategories-list_visible');
+      }
     }));
   }
 }, false);
