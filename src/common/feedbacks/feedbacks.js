3ca566987dcab19695e7d2626246bac4da29b2a9
Vadim <v_a_d@inbox.ru>
2021-12-10 11:54:17 +0300

    add feedbacks block

---
+++ src/common/feedbacks/feedbacks.js
@@ -0,0 +1,16 @@
+/* eslint-disable no-undef */
+import 'owl.carousel/dist/assets/owl.carousel.css';
+import 'owl.carousel';
+
+document.addEventListener('DOMContentLoaded', () => {
+  $('.feedbacks__slider .owl-carousel').owlCarousel({
+    loop: true,
+    autoplay: false,
+    margin: 0,
+    nav: true,
+    navText: false,
+    dots: false,
+    items: 1,
+    navClass: ['feedbacks__nav-button', 'feedbacks__nav-button feedbacks__nav-button_next'],
+  });
+}, false);
