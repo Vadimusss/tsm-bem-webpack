b63bd1978307372a2cde128ab933b8361101c6df
Vadim <v_a_d@inbox.ru>
2021-12-13 15:24:52 +0300

    add target attribute processing

--- src/common/page/page.js
+++ src/common/page/page.js
@@ -12,6 +12,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const title = $(this).text() ? `title="${$(this).text()}"` : '';
     const content = $(this).html();
     const id = this.id ? `id="${this.id}"` : '';
-    return `<a ${href} ${tagClass} ${title} ${id}>${content}</a>`;
+    const target = $(this).data('target') ? `target="${$(this).data('target')}"` : '';
+    return `<a ${href} ${tagClass} ${title} ${id} ${target}>${content}</a>`;
   });
 }, false);
