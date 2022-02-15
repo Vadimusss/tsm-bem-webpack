442293f0ce88de6cf9a28ce6dae2e2e0d49d6cc9
Vadim <v_a_d@inbox.ru>
2022-02-15 16:09:23 +0300

    update about template

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,5 +1,7 @@
 import '../common/header/header.css';
 import '../common/button/button.css';
+import '../common/menu/menu.css';
 
 import './about.html';
+import '../fonts/shop-fonts.css';
 import '../fonts/fonts.css';
