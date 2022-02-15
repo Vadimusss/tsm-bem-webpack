37e27253adecb056f365f2ecdd3b628aeb7a0963
Vadim <v_a_d@inbox.ru>
2022-02-15 16:12:56 +0300

    update main page template

--- src/pages/main.js
+++ src/pages/main.js
@@ -1,7 +1,9 @@
 import '../common/header/header.css';
 import '../common/button/button.css';
+import '../common/menu/menu.css';
 
 import './main.html';
+import '../fonts/shop-fonts.css';
 import '../fonts/fonts.css';
 
 import '../img/brands_buderus.jpg';
