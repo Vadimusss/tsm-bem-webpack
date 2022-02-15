0449a53f0243ffab339b0e4073b48d897f585635
Vadim <v_a_d@inbox.ru>
2022-02-15 16:14:42 +0300

    update prices page template

--- src/pages/prices.js
+++ src/pages/prices.js
@@ -1,5 +1,7 @@
 import '../common/header/header.css';
 import '../common/button/button.css';
+import '../common/menu/menu.css';
 
 import './prices.html';
 import '../fonts/fonts.css';
+import '../fonts/shop-fonts.css';
