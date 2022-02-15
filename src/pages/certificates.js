4f4cd506c4d691a3d64639de6c70d161dfda079a
Vadim <v_a_d@inbox.ru>
2022-02-15 16:10:29 +0300

    update certificates page template

--- src/pages/certificates.js
+++ src/pages/certificates.js
@@ -1,5 +1,7 @@
 import '../common/header/header.css';
 import '../common/button/button.css';
+import '../common/menu/menu.css';
 
 import './certificates.html';
 import '../fonts/fonts.css';
+import '../fonts/shop-fonts.css';
