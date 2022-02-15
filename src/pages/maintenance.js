c5363d8531f374444873db259b9eed47578981d8
Vadim <v_a_d@inbox.ru>
2022-02-15 16:13:22 +0300

    update maintenance page template

--- src/pages/maintenance.js
+++ src/pages/maintenance.js
@@ -1,8 +1,10 @@
 import '../common/header/header.css';
 import '../common/button/button.css';
+import '../common/menu/menu.css';
 
 import './maintenance.html';
 import '../fonts/fonts.css';
+import '../fonts/shop-fonts.css';
 
 import '../img/brands_buderus.jpg';
 import '../img/brands_baxi.jpg';
