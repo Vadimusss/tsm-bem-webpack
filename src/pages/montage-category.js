3b3a6187c4ed5cf6cf547746afec63bf99019e84
Vadim <v_a_d@inbox.ru>
2022-02-15 16:13:48 +0300

    update montage-category page template

--- src/pages/montage-category.js
+++ src/pages/montage-category.js
@@ -1,8 +1,10 @@
 import '../common/header/header.css';
 import '../common/button/button.css';
 import '../common/portfolio/portfolio.css';
+import '../common/menu/menu.css';
 
 import './montage-category.html';
 import '../fonts/fonts.css';
+import '../fonts/shop-fonts.css';
 
 import '../img/services_fon_montage.png';
