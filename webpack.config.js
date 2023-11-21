fceffd05ec5bfeb84d26c9ed1e7f47e922032268
Vadim <v_a_d@inbox.ru>
2023-11-21 16:12:23 +0300

    fix add svg to bundle as font

--- webpack.config.js
+++ webpack.config.js
@@ -100,7 +100,7 @@ module.exports = (env, argv) => {
           },
         },
         {
-          test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+          test: /\.(woff(2)?|ttf|eot)(\?v=\d+\.\d+\.\d+)?$/,
           loader: 'file-loader',
           options: {
             name: '[name].[ext]',
