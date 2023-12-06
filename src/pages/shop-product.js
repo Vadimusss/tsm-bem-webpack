eacfcc4f642ef429f903c8d8ceb95fe4adf5f702
Vadim <v_a_d@inbox.ru>
2023-12-06 10:47:53 +0300

    update block import

--- src/pages/shop-product.js
+++ src/pages/shop-product.js
@@ -2,7 +2,7 @@ import '../common/header/header.scss';
 import '../common/button/button.scss';
 import '../common/menu/menu.scss';
 import '../common/location/location.scss';
-import '../common/services/services.css';
+import '../common/services/services.scss';
 import '../common/callback/callback.scss';
 import '../common/breadcrumbs/breadcrumbs.scss';
 import '../shop/breadcrumbs/breadcrumbs.scss';
