dc0e5c01c95a451382addc30d389377c71816276
Vadim <v_a_d@inbox.ru>
2023-12-06 10:52:05 +0300

    fix multiple map load

--- src/common/location/location.js
+++ src/common/location/location.js
@@ -7,12 +7,15 @@ document.addEventListener('DOMContentLoaded', () => {
     capture: true,
   };
 
-  const mapLoaded = false;
+  let mapNotLoaded = true;
   function startLazyMap() {
-    if (!mapLoaded) {
+    if (mapNotLoaded) {
       const script = document.createElement('script');
       script.setAttribute('src', 'https://api-maps.yandex.ru/services/constructor/1.0/js/?um=constructor%3A78a400cbcda7e37b2c9a9ec3c29af9c64501f52042225081143d14e34e2f1be1&amp;width=100%25&amp;height=100%&amp;lang=ru_RU&amp;scroll=false');
       mapContainer.appendChild(script);
+      mapNotLoaded = false;
+      mapContainer.removeEventListener('mouseover', startLazyMap, optionsMap);
+      mapContainer.removeEventListener('touchstart', startLazyMap, optionsMap);
     }
   }
 
